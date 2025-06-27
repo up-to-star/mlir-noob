0691532909126d8ab62e39d05f6ee08f7a3e98ce
up-to-star <2200316527@qq.com>
2025-06-27 12:10:19 +0800

    refactor(mlir-toy): 更新项目结构和 dialect 定义
    - 注释掉 CMakeLists.txt 中的 dialect 和 north_star_dialect 目录
    - 添加 north_star_type 目录
    - 修改 ToyOps.td 中的 ConstantOp 定义，统一代码格式

---
+++ mlir-toy/north_star_type/include/Dialect/NorthStar/NorthStarDialect.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "mlir/IR/MLIRContext.h"
+#include "mlir/Dialect/Tensor/IR/Tensor.h"
+#include "Dialect/NorthStar/NorthStarDialect.h.inc"
+
+
---
+++ mlir-toy/north_star_type/src/Dialect/NorthStar/NorthStarDialect.cpp
@@ -0,0 +1,20 @@
+#include "Dialect/NorthStar/NorthStarDialect.h"
+#include "llvm/Support/raw_ostream.h"
+#include "Dialect/NorthStar/NorthStarDialect.cpp.inc"
+
+namespace mlir::north_star {
+    void NorthStarDialect::initialize() {
+        llvm::outs() << "initializing " << getDialectNamespace() << "\n";
+        registerType();
+    }
+
+    // 实现方言的析构函数
+    NorthStarDialect::~NorthStarDialect() {
+    llvm::outs() << "destroying " << getDialectNamespace() << "\n";
+    }
+
+    // 实现在extraClassDeclaration 声明当中生命的方法。
+    void NorthStarDialect::sayHello() {
+    llvm::outs() << "Hello in " << getDialectNamespace() << "\n";
+    }
+}
